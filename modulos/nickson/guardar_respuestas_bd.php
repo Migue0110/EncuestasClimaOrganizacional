17e25d7f1b35bdc86442db646a730f1366b0f33a
nickson151 <ngomez72@uan.edu.co>
2023-11-22 19:10:23 -0500

    subir clase

--- modulos/nickson/guardar_respuestas_bd.php
+++ modulos/nickson/guardar_respuestas_bd.php
@@ -1,18 +1,24 @@
 <?php
 include("../db_connection.php");
 
+// Activar reporte de errores
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
 if (isset($_POST['respuesta']) && is_array($_POST['respuesta'])) {
-    // Si existe y es un array, entonces procedemos a contar y procesar las respuestas
-    $tamano = count($_POST['respuesta']);
-    for ($i = 0; $i < $tamano; $i++) {
-        if (isset($_POST['bancopreguntas_idPregunta'][$i], $_POST['empleado_idEmpleado'])) {
-            // Asegúrate de que la pregunta y el ID del empleado estén establecidos antes de intentar insertar
-            insertar_pregunta($conn, $_POST['respuesta'][$i], $_POST['empleado_idEmpleado'], $_POST['bancopreguntas_idPregunta'][$i]);
+    foreach ($_POST['respuesta'] as $idPregunta => $valorRespuesta) {
+        if (isset($_POST['empleado_idEmpleado'])) {
+            // Intenta insertar la pregunta y captura cualquier posible error
+            $resultado = insertar_pregunta($conn, $valorRespuesta, $_POST['empleado_idEmpleado'], $idPregunta);
+            if ($resultado) {
+                echo "Pregunta con ID $idPregunta insertada con éxito.<br>";
+            }
+        } else {
+            echo "ID de empleado no proporcionado.<br>";
         }
     }
 } else {
-    // Manejar el caso en que no se reciben respuestas
-    echo "No se recibieron respuestas.";
+    echo "No se recibieron respuestas o el formato no es correcto.<br>";
 }
 
 function insertar_pregunta($conn, $respuesta, $empleado_idEmpleado, $bancopreguntas_idPregunta) {
@@ -20,14 +26,17 @@ function insertar_pregunta($conn, $respuesta, $empleado_idEmpleado, $bancopregun
     if ($stmt = $conn->prepare($sql)) {
         $stmt->bind_param("iii", $respuesta, $empleado_idEmpleado, $bancopreguntas_idPregunta);
         if ($stmt->execute()) {
-            echo "Pregunta insertada con éxito.";
+            return true;
         } else {
-            echo "Error al insertar pregunta: " . $stmt->error;
+            echo "Error al insertar pregunta: " . $stmt->error . "<br>";
+            return false;
         }
         $stmt->close();
     } else {
-        echo "Error al preparar la declaración: " . $conn->error;
+        echo "Error al preparar la declaración: " . $conn->error . "<br>";
+        return false;
     }
 }
 
 $conn->close();
+?>
